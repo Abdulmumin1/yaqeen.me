4919ab3e6d611089bd5864b54fd5d9ef1f10057f
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-06-21 18:34:25 +0100

    blog

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="w-full flex justify-center py-12">
 	<div class="max-w-[800px] lg:text-xl px-4">
-		<h1 class="text-5xl font-bold p-6 mb-3 font-visby_bold text-center">
+		<h1 class="text-4xl lg:text-5xl font-bold p-6 mb-3 font-visby_bold text-center">
 			{@html data.title}
 		</h1>
 		{@html data.content}
