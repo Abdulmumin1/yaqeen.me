5cc278e0e148eea0b1c3f533f45d656fd62d3145
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-06-22 17:45:28 +0100

    p

--- src/components/nav/nav.svelte
+++ src/components/nav/nav.svelte
@@ -32,7 +32,7 @@
 
 <nav class="w-full flex justify-center items-center flex-col pt-3 font-bold">
 	<ul
-		class="flex border px-2 border-black dark:border-[#333] border-b-4 space-x-2 p-1 rounded-lg items-center justify-center"
+		class="flex border px-2 border-black dark:border-[#333] border-b-4 space-x-2 rounded-lg items-center justify-center"
 	>
 		<NavItem data={{ name: 'Home', href: '/' }} />
 		<p>·</p>
