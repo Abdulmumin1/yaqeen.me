1249c87674bf530996f49e6163baf0f4819c2127
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2023-06-30 19:03:22 +0100

    share

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,30 @@
 <script>
 	export let data;
 	import { formatDate } from '$lib/js/utils.js';
+
+	import { page } from '$app/stores';
 	import Fa from 'svelte-fa';
-	import { faLinkedin, faReddit, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+	import {
+		faFacebook,
+		faHackerNews,
+		faLinkedin,
+		faReddit,
+		faTwitter,
+		faWhatsapp
+	} from '@fortawesome/free-brands-svg-icons';
 	import { faCopy } from '@fortawesome/free-solid-svg-icons';
+
+	function copyUrlToClipboard() {
+		var url = window.location.href;
+		navigator.clipboard
+			.writeText(url)
+			.then(() => {
+				console.log('URL copied to clipboard:', url);
+			})
+			.catch((error) => {
+				console.error('Failed to copy URL to clipboard:', error);
+			});
+	}
 </script>
 
 <svelte:head>
@@ -30,10 +51,50 @@
 			<span>Love it? Share it!</span>
 			<div class="flex space-x-3 items-center justify-center">
 				<span><Fa icon={faCopy} /></span>
-				<span><Fa icon={faTwitter} /></span>
-				<span><Fa icon={faLinkedin} /></span>
-				<span><Fa icon={faWhatsapp} /></span>
-				<span><Fa icon={faReddit} /></span>
+				<a
+					href="https://twitter.com/share?url={window.location
+						.href}%3Fref%3Dtwitter-share&amp;text={data.meta.title}"
+					target="_blank"
+					rel="noopener"
+				>
+					<span><Fa icon={faTwitter} /></span>
+				</a>
+				<a
+					href="http://www.reddit.com/submit?url={window.location.href}&amp;title={data.meta.title}"
+					target="_blank"
+					rel="noopener"
+				>
+					<span><Fa icon={faReddit} /></span>
+				</a>
+				<a
+					href="https://www.linkedin.com/cws/share?url={window.location.href}"
+					target="_blank"
+					rel="noopener"
+				>
+					<span><Fa icon={faLinkedin} /></span>
+				</a>
+				<a
+					href="http://news.ycombinator.com/submitlink?u={window.location.href}&amp;t={data.meta
+						.title}"
+					target="_blank"
+					rel="noopener"
+				>
+					<Fa icon={faHackerNews} />
+				</a>
+				<a
+					href="https://www.facebook.com/sharer/sharer.php?u={window.location.href}"
+					target="_blank"
+					rel="noopener"
+				>
+					<Fa icon={faFacebook} />
+				</a>
+				<a
+					href="https://api.whatsapp.com/send?text={data.meta.title}%20{window.location.href}"
+					target="_blank"
+					rel="noopener"
+				>
+					<button on:click={copyUrlToClipboard}><Fa icon={faWhatsapp} /></button>
+				</a>
 			</div>
 		</div>
 	</div>
