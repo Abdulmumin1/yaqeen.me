92877a2ce1e1d6f1b7f44dc2f2907b0f97a7b5ed
Abdulmumin Yaqeen <avdorr12345@gmail.com>
2024-07-31 10:55:26 +0100

    fix(commentrig): version

---
+++ src/routes/commentrig/demo/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { Comment } from '@commentrig/svelte';
+	import '@commentrig/core/styles/default.css';
+	let entities = {
+		title: 'CommentRig Demo Page',
+		email: 'abdulmuminyqn@gmail.com',
+		unique_id: 'commentrig-demo-page',
+		rich: true,
+		page: 100,
+		domain: 'https://www.yaqeen.me'
+	};
+</script>
+
+<div class="p-12">
+	<Comment {entities} />
+</div>
+
+<style>
+	:root {
+		--rig-text-color: #404953;
+		--rig-background-color: #0c0a09;
+		--rig-color-primary: #c2410c;
+		--rig-inactive-color: #78716c;
+	}
+</style>
