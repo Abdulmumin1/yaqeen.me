4eca1bfcba6b360008991d9f91409539de4f878d
Abdulmumin1 <abdulmuminyqn@gmail.com>
2025-05-04 19:14:18 +0100

    fix: i don't know

--- src/components/general/seo.svelte
+++ src/components/general/seo.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { page } from '$app/stores';
 
 	/**
 	 * @typedef {Object} Props
@@ -25,40 +26,37 @@
 	} = $props();
 
 	let truncatedDescription = $derived(description.slice(0, 300));
-	let ogImage = $derived(image || ``);
-
-	// let ogImage = $derived(image || `${$page.url.origin}/og?message=${encodeURIComponent(title)}`);
+	let ogImage = $derived(image || `${$page.url.origin}/og?message=${encodeURIComponent(title)}`);
 </script>
 
-<svelte:head>
-	<title>{title}</title>
-	<meta name="description" content={truncatedDescription} />
+<title>{title}</title>
+<meta name="description" content={truncatedDescription} />
 
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content={type} />
-	<meta property="og:title" content={title} />
-	<meta property="og:description" content={truncatedDescription} />
-	<meta property="og:image" content={ogImage} />
-	<meta property="og:site_name" content={siteName} />
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content={type} />
+<meta property="og:url" content={$page.url} />
+<meta property="og:title" content={title} />
+<meta property="og:description" content={truncatedDescription} />
+<meta property="og:image" content={ogImage} />
+<meta property="og:site_name" content={siteName} />
 
-	<!-- Twitter -->
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:site" content={twitterHandle} />
-	<meta name="twitter:title" content={title} />
-	<meta name="twitter:description" content={truncatedDescription} />
-	<meta name="twitter:image" content={ogImage} />
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content={twitterHandle} />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={truncatedDescription} />
+<meta name="twitter:image" content={ogImage} />
 
-	<!-- Other important meta tags -->
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta http-equiv="content-language" content="en-us" />
+<!-- Other important meta tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="content-language" content="en-us" />
 
-	{#if published}
-		<meta name="robots" content="index, follow" />
-	{:else}
-		<meta name="robots" content="noindex, nofollow" />
-	{/if}
+{#if published}
+	<meta name="robots" content="index, follow" />
+{:else}
+	<meta name="robots" content="noindex, nofollow" />
+{/if}
 
-	{#if canonical}
-		<link rel="canonical" href={canonical} />
-	{/if}
-</svelte:head>
+{#if canonical}
+	<link rel="canonical" href={canonical} />
+{/if}
