946ea9574bec772c95c0150f7b3f25b2039e7c5c
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2025-12-22 18:27:20 +0100

    chore: added tiny.auth project

--- src/lib/utils/projectStore.js
+++ src/lib/utils/projectStore.js
@@ -113,6 +113,20 @@ export const project_data = readable([
 			'https://rawcontent.dearfutureself.me/portfolio/Screenshot%202025-11-23%20at%2021.29.52.png'
 		]
 	},
+
+		{
+		name: 'tiny.auth',
+		stack: ['bunjs', 'ink', 'hash-algorithm'],
+		description: 'A TOTP client for the CLI',
+		links: {
+			study: 'https://github.com/Abdulmumin1/tiny.authenticator',
+		},
+		imagelist: [
+			'https://rawcontent.dearfutureself.me/portfolio/tiny-uth-image-2.png',
+			'https://rawcontent.dearfutureself.me/portfolio/tiny.auth-image-1.png',
+		]
+	},
+	
 	{
 		name: 'LMFetch',
 		stack: ['Python', 'Coroutines', 'Thread pool'],
