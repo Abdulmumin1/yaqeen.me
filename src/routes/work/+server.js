4fc50b6c2301c138e6424db4441f59cf0618a825
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2026-02-11 21:59:33 +0100

    ui: rivet realtime in /garden

--- src/routes/work/+server.js
+++ src/routes/work/+server.js
@@ -2,3 +2,9 @@
 async function load() {
     throw redirect(302, '/projects');
 }
+import { redirect } from '@sveltejs/kit';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
+    throw redirect(308, '/projects');
+}
