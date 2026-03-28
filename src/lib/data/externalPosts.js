3b7082cb805617c665c33d9487116c34fa78f377
Abdulmumin Yaqeen <abdulmuminyqn@gmail.com>
2026-03-28 07:53:24 +0100

    read: lost in truncation

--- src/lib/data/externalPosts.js
+++ src/lib/data/externalPosts.js
@@ -8,4 +8,26 @@ export const externalPosts = [
 	// 	url: 'https://example.com/post',
 	// 	categories: ['writing']
 	// }
+	{
+		title: 'Tool Shaped Objects - Will Manidis',
+		description: 'Ask what the number is before making it go up.',
+		date: '2026-02-11',
+		url: 'https://minutes.substack.com/p/tool-shaped-objects',
+		categories: ['reads']
+	},
+	// {
+	// 	title: "People really don't like metered billing - Ayush (autumn)",
+	// 	description: 'People don’t like usage-based billing because they can’t predict it.',
+	// 	date: '2026-02-13',
+	// 	url: 'https://useautumn.com/blog/usage-billing-bad',
+	// 	categories: ['writing']
+	// },
+
+	{
+		title: 'Lost in Truncation',
+		description: 'Jean-Baptiste Lamarck’s Revenge',
+		date: '2026-03-26',
+		url: 'https://www.whitenoise.email/p/lost-in-truncation',
+		categories: ['writing']
+	}
 ];
